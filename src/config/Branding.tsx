14e42782e41edf778b93daa911892dbf3af6a505
grwthwrx-gif <your-grwthwrx@gmail.com>
2025-09-28 19:36:07 +0100

    Add Branding.tsx central config for Psynk assets

---
+++ src/config/Branding.tsx
@@ -0,0 +1,20 @@
+export const Branding = {
+  images: {
+    favicon: require("../../assets/images/psynk/favicon.ico"),
+    logoFull: require("../../assets/images/psynk/logo_full.png"),
+    logoIcon: require("../../assets/images/psynk/logo_icon.png"),
+    logoBW: require("../../assets/images/psynk/logo_bw.png"),
+    logoReversed: require("../../assets/images/psynk/logo_reversed.png"),
+  },
+  animations: {
+    psynk: require("../../assets/animations/psynk/psynk.json"),
+    psynkDark: require("../../assets/animations/psynk/psynk_dark.json"),
+  },
+  colors: {
+    backgroundDark: "#0B0C10",
+    backgroundLight: "#F0F0F0",
+    accentCyan: "#00FFF7",
+    accentMagenta: "#FF2975",
+    textPrimary: "#FFFFFF",
+  },
+};
